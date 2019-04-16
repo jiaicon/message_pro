335cbcca73659602693c8139a0de668da5a3c388
jiaicon@163.com <jiaicon@163.com>
2019-04-16 16:55:46 +0800

    init

---
+++ src/Message/Publish/Tools.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: icon
+ * Date: 2019/4/16
+ * Time: 下午4:26
+ */
+namespace Message\Publish;
+
+
+class Tools {
+    public function __construct()
+    {
+
+    }
+    public static function Hello() {
+        echo "hello world";
+    }
+}
